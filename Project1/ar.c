1f1c48d1c9d9e7323bba9542bbce3eb19084cbde
hsem4717 <hsem4717@gmail.com>
2022-06-28 11:50:58 +0900

    Create ar.c

---
+++ Project1/ar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    if (a == 1 || a == 3 || a == 5 || a == 7)
+    {
+        printf("oh my god");
+    }
+    else
+    {
+        printf("enjoy");
+    }
+    return 0;
+}
