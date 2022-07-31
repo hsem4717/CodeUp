7e339a6a872860f5ce8fd6bdd92cfec8dc0c531a
hsem4717 <hsem4717@gmail.com>
2022-07-31 20:19:05 +0900

    Create zig.c

---
+++ Project1/zig.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() {
+    int h, r;
+    scanf("%d %d", &h, &r);
+    for (int i = 1; i <= r; i++) {
+        int m = 1;
+        for (int j = 1; j <= h * 2 - 1; j++) {
+            if (j <= h) {
+                for (int k = 1; k <= h; k++) {
+                    if (k == j) {
+                        printf("*");
+                    }
+                    else {
+                        printf(" ");
+                    }
+                }
+            }
+            else {
+                for (int k = 1; k <= h - 1; k++) {
+                    if (k == j - 2 * m) {
+                        printf("*");
+                    }
+                    else {
+                        printf(" ");
+                    }
+                }
+                m++;
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
