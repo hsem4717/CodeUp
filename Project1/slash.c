74a8dd75f874c2a6fd9bd5ce80400abb69703ac3
hsem4717 <hsem4717@gmail.com>
2022-08-22 19:36:08 +0900

    Create slash.c

---
+++ Project1/slash.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    char data[30];
+    scanf("%s", &data);
+    for (i = 0; data[i] != '\0'; i++)
+    {
+        printf("\'%c\'\n", data[i]);
+    }
+}
